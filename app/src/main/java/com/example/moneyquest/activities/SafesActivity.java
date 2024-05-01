588b8c6ee85c1606078655a6b03bc6b6a761192a
Stefane Maria <stefane.maria0901@gmail.com>
2024-05-01 13:07:24 -0300

    Create activities

---
+++ app/src/main/java/com/example/moneyquest/activities/SafesActivity.java
@@ -0,0 +1,16 @@
+package com.example.moneyquest.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.moneyquest.R;
+
+public class SafesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safes);
+    }
+}
