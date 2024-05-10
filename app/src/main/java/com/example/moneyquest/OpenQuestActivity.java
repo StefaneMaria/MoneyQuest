3b81eed1385f1e3382e79d02295330f5440734df
Stefane Maria <stefane.maria0901@gmail.com>
2024-05-10 16:14:13 -0300

    Add open quest screen

---
+++ app/src/main/java/com/example/moneyquest/OpenQuestActivity.java
@@ -0,0 +1,14 @@
+package com.example.moneyquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpenQuestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_quest);
+    }
+}
